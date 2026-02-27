dfbf48b0419061ae1836400d3fa0df2bbd4c755b
Angel Lucero <alucero-2021211@kinal.edu.gt>
2026-02-26 23:57:47 -0600

    feat: aplicar middlewares de validación y autenticación en rutas de platillos y empleados

--- src/employees/employee.routes.js
+++ src/employees/employee.routes.js
@@ -1,20 +1,31 @@
 import { Router } from 'express';
 import { uploadMemory } from '../../middlewares/file-uploader.js';
 import { createEmployee, changeEmployeeStatus, getEmployeeById, getEmployees, updateEmployee } from './employee.controller.js';
+import {
+    validateCreateEmployee,
+    validateUpdateEmployee,
+    validateGetEmployeeById,
+    validateChangeEmployeeStatus
+} from '../../middlewares/employees-validators.js';
+import { validateJWT } from '../../middlewares/validate-JWT.js';
 
 const router = Router();
 
-router.get('/', getEmployees);
+router.get('/', validateJWT, getEmployees);
 
-router.get('/:id', getEmployeeById);
+router.get('/:id', validateGetEmployeeById, getEmployeeById);
 
 router.post('/',
     uploadMemory.single('profilePicture'),
+    validateCreateEmployee,
     createEmployee);
 
 router.put('/:id',
+    validateUpdateEmployee,
     updateEmployee);
 
-router.patch('/:id', changeEmployeeStatus);
+router.patch('/:id',
+    validateChangeEmployeeStatus,
+    changeEmployeeStatus);
 
 export default router;
