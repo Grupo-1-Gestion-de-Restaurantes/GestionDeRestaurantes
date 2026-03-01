19ed5393994edb105ca37ace08992185524f5048
SyntaxJeferson <jyaxon-2024442@kinal.edu.gt>
2026-03-01 11:22:31 -0600

    feat: Obtener Reservaciones del Usario Logeado

--- middlewares/syncClient.js
+++ middlewares/syncClient.js
@@ -2,22 +2,31 @@ import Client from '../src/client/client.model.js';
 
 export const syncClient = async (req, res, next) => {
     try {
-        const { id } = req.user; 
+        if (!req.user?.id) {
+            return res.status(400).json({
+                success: false,
+                message: "ID de usuario no disponible"
+            });
+        }
+
+        const { id } = req.user;
 
         const user = await Client.findOneAndUpdate(
-            { _id: id },
-            { 
-                $setOnInsert: { 
-                    _id: id, 
+            { id: id },
+            {
+                $setOnInsert: {
+                    id: id,
                     isActive: true
-                } 
+                }
             },
             { upsert: true, new: true }
         );
 
         req.user = user;
         next();
+
     } catch (error) {
+        console.error(error);
         res.status(500).json({
             success: false,
             message: "Error en la sincronización del cliente",
